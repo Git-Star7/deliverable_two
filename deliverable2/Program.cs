c5bd9cf4ba46101cd644fd45ce8c0da04dc73a17
Michael <Michael@acdmail.com>
2019-12-23 14:34:30 -0500

    base code for message encoder

--- deliverable2/Program.cs
+++ deliverable2/Program.cs
@@ -6,7 +6,20 @@ namespace deliverable2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            string upper = input.ToUpper();
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string message = "";
+            int checksum = 0;
+
+            foreach (char myChar in upper)
+            {
+                message += letters.IndexOf(myChar) + 1 + "-";
+                checksum += System.Convert.ToInt32(myChar);
+            }
+
+            Console.WriteLine(message);
+            Console.WriteLine("Message checksum is " + checksum);
         }
     }
 }
